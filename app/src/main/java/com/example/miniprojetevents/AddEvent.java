911fc31aec80cf00e8d23b31cd10734b6f583862
maher1719 <ba.maher94@gmail.com>
2019-12-08 01:34:02 +0100

    change field

--- app/src/main/java/com/example/miniprojetevents/AddEvent.java
+++ app/src/main/java/com/example/miniprojetevents/AddEvent.java
@@ -1,13 +1,12 @@
 package com.example.miniprojetevents;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddEvent extends AppCompatActivity {
 
-    TextView ab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
